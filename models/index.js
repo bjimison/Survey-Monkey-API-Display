29410ebf5edff7a424649afa409ed948f83996f6
bjimison <brady.t.jimison@gmail.com>
2019-12-01 10:56:28 -0800

    styled html elements, aligned, gave color etc

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost:27017/survey",
     { useNewUrlParser: true },
-    // { useUnifiedTopology: true }
+    { useUnifiedTopology: true }
 );
 
 let Survey = require("./survey.js");
